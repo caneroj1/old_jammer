89101d7c5e1eb75d470992fd8f2d81026bd38937
caneroj1 <caneroj1@tcnj.edu>
2014-07-04 19:03:16 -0400

    added ability to upload profile images and started design of user profile page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,6 @@ class User < ActiveRecord::Base
   validates :first_name,  presence: true
   validates :last_name,   presence: true
   validates :instrument,  presence: true
+
+  mount_uploader :avatar, AvatarUploader
 end
