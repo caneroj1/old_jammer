d1e1fbf863e54b541d0b072a5c7bef9fea938429
caneroj1 <caneroj1@tcnj.edu>
2014-07-04 10:31:17 -0400

    added navbar.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < Devise::RegistrationsController
+	
+	## override the new method for a registration controller in order
+	# to pass new instance variables to the view
 	def new
 		@instrument_list = instrument_list
 		@state_list = state_list
