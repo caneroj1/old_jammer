cd04a4cf248547927775bbc05bc23e03667cecf1
caneroj1 <caneroj1@tcnj.edu>
2014-07-22 19:22:06 -0400

    added more logic to replies to show dates. added timestamps to replies.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722021159) do
+ActiveRecord::Schema.define(version: 20140722231653) do
 
   create_table "messages", force: true do |t|
     t.datetime "created_at"
@@ -31,9 +31,11 @@ ActiveRecord::Schema.define(version: 20140722021159) do
   end
 
   create_table "replies", force: true do |t|
-    t.text    "reply_body"
-    t.integer "sent_by"
-    t.integer "message_id"
+    t.text     "reply_body"
+    t.integer  "sent_by"
+    t.integer  "message_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
