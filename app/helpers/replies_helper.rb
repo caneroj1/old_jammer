52fda88ede32904be53078e645939b4a99c7c658
caneroj1 <caneroj1@tcnj.edu>
2014-07-24 00:31:11 -0400

    worked on sending back more json data to handle any missing replies so instead of always selecting the last reply, the messages pane will get the replies that it does not have.

--- app/helpers/replies_helper.rb
+++ app/helpers/replies_helper.rb
@@ -16,18 +16,18 @@ module RepliesHelper
 		response = what_time_to_show?(reply_time)
 		case response
 		when "minutes"
-			reply_time.strftime("%l:%M,  %P")
+			reply_time.localtime.strftime("%l:%M,  %P")
 		when "months"
-			reply_time.strftime("%-m/%e")
+			reply_time.localtime.strftime("%-m/%e")
 		when "years"
-			reply_time.strftime("%-m/%Y")
+			reply_time.localtime.strftime("%-m/%Y")
 		end
 	end
 
 	# if the time the message was created is more than 5 minutes ago,
 	# we should display the time the reply was created
 	def show_time?(reply)
-		(Time.now - reply.created_at) > 300.0
+		(Time.now - reply.created_at.localtime) > 300.0
 	end
 
 	# if the reply was created at least a day ago, only show the month and date
@@ -35,9 +35,9 @@ module RepliesHelper
 	# else, show minutes and seconds
 	def what_time_to_show?(reply_time)
 		now = Time.now
-		if (now - reply_time) < (60 * 60 * 24)
+		if (now - reply_time.localtime) < (60 * 60 * 24)
 			"minutes"
-		elsif (now - reply_time) < (60 * 60 * 24 * 365)
+		elsif (now - reply_time.localtime) < (60 * 60 * 24 * 365)
 			"months"
 		else
 			"years"
