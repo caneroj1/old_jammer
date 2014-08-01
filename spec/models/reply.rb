c60a660e56c12cfbb8176e044219193c961fb885
caneroj1 <caneroj1@tcnj.edu>
2014-07-31 23:54:18 -0400

    added test for reply model.

---
+++ spec/models/reply.rb
@@ -0,0 +1,17 @@
+require 'faker'
+
+describe Reply do
+	let(:reply) { FactoryGirl.build(:reply) }
+
+	it 'has a valid factory' do
+		expect(reply).to be_valid
+	end
+
+	it 'has a body' do
+		expect(reply.reply_body).to_not eq(nil)
+	end
+
+	it 'has a sent by attribute' do
+		expect(reply.sent_by.class).to eq(Fixnum)
+	end
+end
