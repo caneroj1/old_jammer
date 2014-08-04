3104061dd03b38f9956b2f16fea801dd1d8653fd
caneroj1 <caneroj1@tcnj.edu>
2014-08-03 20:41:36 -0400

    more updates to testing messages controller.

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -28,4 +28,32 @@ describe MessagesController do
 			expect { post :create, musician_id: user.id, message: { subject: good_message.subject, message_body: good_message.message_body, sent_by: good_message.sent_by } }.to_not change(Message, :count)
 		end
 	end
+
+	describe "#get_message" do
+		let (:message) { FactoryGirl.create(:message, :for_jam) }
+		let (:user) { FactoryGirl.create(:user) }
+
+		before :each do
+			sign_in user
+		end
+
+		it 'gets the desired message and renders the partial' do
+			post :get_message, musician_id: user.id, id: message.id, m_id: message.id
+			expect(response).to render_template 'partials/message/_message'
+		end
+	end
+
+	describe "#get_reply" do
+		let (:user) { FactoryGirl.create(:user) }
+		let (:message_with_reply) { FactoryGirl.create(:message_with_reply, :for_jam) }
+		
+		before :each do
+			sign_in user
+		end
+
+		it 'gets the desired reply' do
+			post :get_reply, musician_id: user.id, id: message_with_reply.id, m_id: message_with_reply.id, r_id: message_with_reply.replies[0].id
+			expect(response).to render_template 'partials/replies/_reply'
+		end
+	end
 end
