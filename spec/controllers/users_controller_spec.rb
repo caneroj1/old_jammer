5fd978b3ca47f67491f80fc26ef6b00f61c1ffa3
caneroj1 <caneroj1@tcnj.edu>
2014-08-04 20:59:20 -0400

    more updates to users controller testing.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -37,11 +37,11 @@ describe UsersController do
 	describe "#update" do
 		let (:user) { FactoryGirl.create(:user, first_name: "bob", last_name: "beefcake") }
 		
+		before :each do
+			sign_in user
+		end
+
 		context 'with valid attributes' do
-			before :each do
-				sign_in user
-			end
-			
 			it 'updates the user\'s email' do
 				put :update, user: FactoryGirl.attributes_for(:user, email: "tyrion.lannister@westeros.org")
 				
@@ -49,6 +49,25 @@ describe UsersController do
 				
 				expect(user.email).to eq("tyrion.lannister@westeros.org")
 			end
+
+			it 'redirects to the edit page' do
+				put :update, user: FactoryGirl.attributes_for(:user, email: "tyrion.lannister@westeros.org")
+			
+				expect(response).to redirect_to(edit_user_registration_path)
+			end
+		end
+
+		context 'with invalid attributes' do
+			it 'does not update the user\'s email' do
+				expect { put :update, user: FactoryGirl.attributes_for(:user, email: "") }.to_not change(User, :count)
+			end
+		end
+	end
+
+	describe "#update_password" do
+		let (:user) { FactoryGirl.create(:user) }
+		context 'with valid attributes' do
+
 		end
 	end
 end
